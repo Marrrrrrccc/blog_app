5d6a9bb30376043a08776b5476707b41039921d6
Marc Ricafort <marcricson.ricafort.iics@ust.edu.ph>
2020-07-02 10:34:10 +0800

    init

---
+++ Restful_blogApp/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+//app config
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+//Mongoose/Model config
+const blogSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  body: String,
+  date: { type: Date, default: Date.now },
+});
+const Blog = mongoose.model("Blog", blogSchema);
+
+//Restful routes
+app.get("/", function (req, res) {
+  res.redirect("/blogs");
+});
+//Index
+app.get("/blogs", function (req, res) {
+  Blog.find({}, function (err, blogs) {
+    if (err) {
+      console.log(err);
+    }
+    res.render("index", { blogs: blogs });
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Listening");
+});
